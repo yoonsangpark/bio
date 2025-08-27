79d01a09046f3cb739dfb967f77b6e5c0bc54645
yoonsangpark <yoonsangpark@gmail.com>
2025-08-27 21:54:07 +0900

    black pixel count

--- 08-get_size_grey.py
+++ 08-get_size_grey.py
@@ -11,18 +11,14 @@ if img is not None:
     cv2.imshow('RGB', img)
     cv2.imshow('Gray', gray_img)
 
+    # 1. Grey to Binary
     ret, bin_img = cv2.threshold(gray_img, 127, 255, cv2.THRESH_BINARY) 
     cv2.imshow('Binary', bin_img)
 
-    print(img.shape)
-    print(gray_img.shape)
-    print(bin_img.shape)
+    # 2. black pixel count (black == 0, white == 1)
+    leaf_pixels = np.sum(bin_img == 0)
+    print(f"leaf pixels: {leaf_pixels} px")
 
-    print('gray_img')
-    print(gray_img)
-
-    print('bin_img')
-    print(bin_img)
 
     cv2.waitKey()
     cv2.destroyAllWindows()
